3edb282d51e6658d61d1f328f7fee60aabf305ce
unknown <smitherd9@gmail.com>
2021-03-11 17:13:54 -0500

    changed logo styles

--- src/header.js
+++ src/header.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Grid, Row, Col, Navbar, Nav, NavItem, NavDropdown, MenuItem, Modal, Button } from 'react-bootstrap';
+import baliMask from './img/bali_mask.jpg'
 
 function Header() {
     return (
@@ -10,12 +11,14 @@ function Header() {
                </div>
                <div className="logo">
                    BALI GAMELAN MAP
-                   <div className="logo-img">
-                   <a href="#"><img src="./img/barong-bw.gif" /></a>
+                <div className="logo-img">
+                   <a href="#"><img className="mask-logo" src={baliMask}/></a>                  
+                </div>
+                        
                    </div>
-                  
-               </div>
+
                <div className="menu">
+
                   <ul>
                      <li><a href="#">Home</a></li>
                      <li><a href="#">About</a></li>
@@ -24,7 +27,9 @@ function Header() {
                      <li><a href="#">Contact</a></li>
                      <li><a href="#">Contact</a></li>
                   </ul>
+                  
                </div>
+
             </Nav>
 
         </div>
